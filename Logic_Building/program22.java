9835e75d604cd6278c6fb44ac3304ce0b61a6657
Shreya-Pramod-Pasalkar <shreyappasalkar04@gmail.com>
2026-04-29 09:21:44 +0530

    Iteration and Sequence implementation

---
+++ Logic_Building/program22.java
@@ -0,0 +1,102 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//  Required Header Files
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner; 
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name :   Implement
+//  Description :  It is used to implement Iteration and Sequence
+//  Author :       Shreya Pramod Pasalkar
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+class Implement
+{
+    public int iNo = 0;
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function :     Constructor
+    //  Description :  Parameterized constructor
+    //  Input :        Integer
+    //  Output :       Nothing
+    //  Author :       Shreya Pramod Pasalkar  
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////
+    public Implement(int A)
+    {
+        this.iNo = A;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function :     ImplementIteration
+    //  Description :  It is used to implement Iteration ~ for loop
+    //  Input :        Nothing
+    //  Output :       Nothing
+    //  Author :       Shreya Pramod Pasalkar  
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////
+    public void ImplementIteration()
+    {   
+        int iCnt = 0;
+        // Iteration
+        for(iCnt = 0; iCnt < iNo; iCnt++)
+        {
+            System.out.println("Jay Ganesh...");
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function :     ImplementSequence
+    //  Description :  It is used to implement Sequence
+    //  Input :        Nothing
+    //  Output :       Nothing
+    //  Author :       Shreya Pramod Pasalkar  
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////
+    public void ImplementSequence()
+    {   
+        // Sequence
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name :   program22
+//  Description :  Entry point class
+//  Author :       Shreya Pramod Pasalkar
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+class program22
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = 0;
+
+        System.out.println("Enter Number : ");
+        iNo = sobj.nextInt();
+
+        // Filter
+        if(iNo < 0)
+        {
+            System.out.println("Invalid Input : Number should be Positive");
+            return;
+        }
+
+        Implement iobj = new Implement(iNo);
+        
+        System.out.println("------------------- Sequence -------------------");
+        iobj.ImplementSequence();
+
+        System.out.println("------------------ Iteration -------------------");
+        iobj.ImplementIteration();
+    }
+}
